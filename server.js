3b22a846229d3fa860194554872cae69f2c2d1a7
A00278612 <A00278612@student.ait.ie>
2021-03-23 18:21:55 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.port;
+
+
+let data = {};
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+    res.send(data);
+});
+
+
+
+
+app.post('/data', (req, res) => {
+   const {dth11Error,soil,humidity,temperature} = req.body;
+   data = {dth11Error: dth11Error, SoilMoisture: soil, Humidity: humidity, Temperature: temperature}
+   res.send(`error is ${dth11Error}: soil is ${soil}: humidity is ${humidity}: temperature is ${temperature}`)
+})
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
