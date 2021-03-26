6f078b5da8eb83345dd395dd7e93eec45715aeb2
A00278612 <A00278612@student.ait.ie>
2021-03-26 01:39:49 +0000

    fixing post

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
 
 app.post('/data', (req, res) => {
    const {dth11Error,soil,humidity,temperature} = req.body;
-   db('plantData').insert({dth11Error: dth11Error, SoilMoisture: soil, Humidity: humidity, Temperature: temperature})
+   db('plantdata').insert({dth11Error: dth11Error, SoilMoisture: soil, Humidity: humidity, Temperature: temperature})
    .then(data => console.log)
    res.send('Server recevied the Data')
 })
