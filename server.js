2e16233cac1a0cdae9086ac2f3befbed6a2ac4ed
A00278612 <A00278612@student.ait.ie>
2021-03-26 01:04:10 +0000

    add errror

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
-    db.select('*').from('plantData').then(data => res.json(data));
+    db.select('*').from('plantData').then(data => res.json(data)).catch(err => res.send(err));
 });
 
 app.post('/data', (req, res) => {
